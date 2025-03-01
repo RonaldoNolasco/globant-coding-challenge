c2fdd8c112c80db049a96f8997a2a64e4d19cf2f
Ronaldo Nolasco Chavez <ronaldonolascochavez@gmail.com>
2025-02-28 20:55:02 -0500

    updating database.py, changing data root

--- app/database.py
+++ app/database.py
@@ -1,22 +1,22 @@
 from sqlmodel import SQLModel, Session, create_engine
 import os
+from dotenv import load_dotenv
 
-# Obtener el nombre del archivo de la base de datos desde la variable de entorno
-DB_FILENAME = os.getenv("DATABASE_NAME", "database.db")
-DATABASE_URL = f"sqlite:///./{DB_FILENAME}"  # Construir la URL de conexión
+# Cargar variables de entorno
+load_dotenv()
 
-# Si se usa SQLite, eliminar la BD existente para recrearla
-""" if os.path.exists(DB_FILENAME):
-    os.remove(DB_FILENAME) """
+# Obtener el nombre del archivo de la BD desde .env
+DATABASE_FILE = os.getenv("DATABASE_FILE", "data/database.db")
+DATABASE_URL = f"sqlite:///{DATABASE_FILE}"  # Puedes cambiar a otro motor de BD si es necesario
 
 # Crear el motor de la base de datos
 engine = create_engine(DATABASE_URL, echo=True)
 
 # Función para inicializar la base de datos
-def create_db_and_tables():
+def init_db():
     SQLModel.metadata.create_all(engine)
 
-# Dependencia para obtener la sesión de la base de datos
+# Obtener sesión
 def get_session():
     with Session(engine) as session:
         yield session
