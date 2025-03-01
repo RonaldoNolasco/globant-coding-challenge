6557e4706bd44d17e4697642e47ae5740be703b1
Ronaldo Nolasco Chavez <ronaldonolascochavez@gmail.com>
2025-03-01 10:36:59 -0500

    adding test as module

--- test/test_crud.py
+++ test/test_crud.py
@@ -1,5 +1,5 @@
 from app.models import Department, Job, Employee
-from app.test_setup import test_session, setup_test_db  # Importa el fixture manualmente
+from test.test_setup import test_session, setup_test_db
 from sqlmodel import Session, select
 
 # Department
