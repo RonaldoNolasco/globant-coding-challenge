f482f4b46863a93a4c8c6b4be1ee262efb14a1dd
Ronaldo Nolasco Chavez <ronaldonolascochavez@gmail.com>
2025-03-02 13:05:06 -0500

    adding redirecting to swagger

--- app/main.py
+++ app/main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI
 from app.database import init_db
 from app.routes import router
 from contextlib import asynccontextmanager
+from fastapi.responses import RedirectResponse
 
 # Crear la aplicación FastAPI
 app = FastAPI()
+
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
 
 # Inicializar la base de datos al inicio
 @app.on_event("startup")
