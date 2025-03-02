f053d5f77be0a8e24966eea777f17b3ca7005b3f
Ronaldo Nolasco Chavez <ronaldonolascochavez@gmail.com>
2025-03-02 11:51:43 -0500

    updating tests

--- test/test_api.py
+++ test/test_api.py
@@ -8,23 +8,23 @@ client = TestClient(app)
 def test_upload_departments_csv():
     csv_content = b"1,Finance\n2,Engineering\n3,HR"
     files = {"file": ("departments.csv", io.BytesIO(csv_content), "text/csv")}
-    response = client.post("/upload_csv/", files=files)
+    response = client.post("/upload/departments/", files=files)
     assert response.status_code == 200
-    assert response.json() == {"message": "Data uploaded successfully"}
+    assert "inserted" in response.json()
 
 def test_upload_jobs_csv():
     csv_content = b"1,Analyst\n2,Developer\n3,Manager"
     files = {"file": ("jobs.csv", io.BytesIO(csv_content), "text/csv")}
-    response = client.post("/upload_csv/", files=files)
+    response = client.post("/upload/jobs/", files=files)
     assert response.status_code == 200
-    assert response.json() == {"message": "Data uploaded successfully"}
+    assert "inserted" in response.json()
 
 def test_upload_employees_csv():
     csv_content = b"1,John Doe,2021-03-15,1,2\n2,Jane Doe,2021-06-20,2,3"
-    files = {"file": ("hired_employees.csv", io.BytesIO(csv_content), "text/csv")}
-    response = client.post("/upload_csv/", files=files)
+    files = {"file": ("employees.csv", io.BytesIO(csv_content), "text/csv")}
+    response = client.post("/upload/employees/", files=files)
     assert response.status_code == 200
-    assert response.json() == {"message": "Data uploaded successfully"}
+    assert "inserted" in response.json()
 
 def test_get_employees_per_quarter():
     response = client.get("/employees_per_quarter/")
@@ -35,4 +35,3 @@ def test_get_departments_above_mean():
     response = client.get("/departments_above_mean/")
     assert response.status_code == 200
     assert isinstance(response.json(), list)  # Debe devolver una lista
-
