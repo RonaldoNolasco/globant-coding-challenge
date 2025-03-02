ae24cddab8f4501b3162914bb5416052ae3d9eb6
Ronaldo Nolasco Chavez <ronaldonolascochavez@gmail.com>
2025-03-02 15:51:36 -0500

    editing how to handle dev and test DATABASE_URL

--- app/database.py
+++ app/database.py
@@ -1,26 +1,20 @@
 import os
 from sqlmodel import SQLModel, Session, create_engine
 from dotenv import load_dotenv
+import logging
+logger = logging.getLogger('uvicorn.error')
 
 load_dotenv()
 
 # Si la variable TESTING está en "true", usar BD en memoria
-TESTING = os.getenv("TESTING", "false").lower() == "true"
-
-if TESTING:
-    DATABASE_URL = "sqlite:///db/test_database.db"
-else:
-    DATABASE_URL = "sqlite:///db/database.db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///db/database.db")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 def init_db():
-    if TESTING:
-        SQLModel.metadata.drop_all(engine)
-        SQLModel.metadata.create_all(engine)
-    else:
-        #SQLModel.metadata.drop_all(engine)
-        SQLModel.metadata.create_all(engine)
+    logger.info(DATABASE_URL)
+    SQLModel.metadata.drop_all(engine)
+    SQLModel.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
